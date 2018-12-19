04b174c21566ea56c0c66b94f260560ccb51685a
Manuel Garcia <manuel.drupal@gmail.com>
2018-12-19 11:20:30 +0000

    Fix CS in BtcPayInterface

--- src/Plugin/Commerce/PaymentGateway/BtcPayInterface.php
+++ src/Plugin/Commerce/PaymentGateway/BtcPayInterface.php
@@ -14,7 +14,7 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
   /**
    * Gets the server URL.
    *
-   * @return array $host
+   * @return array
    *   Returns an array with host and port info: 1 => 'host', 2 => 'port'.
    *   Defaults to port 443.
    */
@@ -23,22 +23,20 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
   /**
    * Creates and saves a key pair and token.
    *
-   * @copyright Code heavily inspired from BitPay module: https://dgo.to/bitpay
-   * @author Mark Burdett (mfb)
-   *
    * @param string $network
    *   Network string we want to configure.
    * @param string $pairing_code
    *   Pairing code provided by BTCPayServer.
    *
-   * @return void
+   * @copyright Code heavily inspired from BitPay module: https://dgo.to/bitpay
+   * @author Mark Burdett (mfb)
    */
   public function createToken($network, $pairing_code);
 
   /**
    * Instantiate and return REST API Client.
    *
-   * @return \Bitpay\Client\Client|NULL;
+   * @return \Bitpay\Client\Client|null
    *   Returns the client or NULL.
    */
   public function getBtcPayClient();
@@ -64,7 +62,7 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
    * @param string $invoiceId
    *   The remote invoice ID.
    *
-   * @return \Bitpay\Invoice|NULL
+   * @return \Bitpay\Invoice|null
    *   The queried invoice or NULL.
    */
   public function getInvoice($invoiceId);
@@ -73,30 +71,34 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
    * Check BTCPay server invoice and check status.
    *
    * @param \Bitpay\InvoiceInterface $invoice
+   *   Remote BTCPay invoice.
    *
-   * @return boolean
+   * @return bool
+   *   Whether or not the invoice was paid in full.
    */
-  public function checkInvoicePaidFull($invoice);
+  public function checkInvoicePaidFull(InvoiceInterface $invoice);
 
   /**
    * Check BTCPay server invoice on payment error states.
    *
    * @param \Bitpay\InvoiceInterface $invoice
+   *   Remote BTCPay invoice.
    *
-   * @return boolean
+   * @return bool
+   *   Whether or not the invoice payment failed.
    */
-  public function checkInvoicePaymentFailed($invoice);
+  public function checkInvoicePaymentFailed(InvoiceInterface $invoice);
 
   /**
-   * Check if a payment entity for a given order and invoice combination already
-   * exists and return it or NULL if not.
+   * Check if a payment entity for a given order and invoice combination exists.
    *
    * @param \Drupal\commerce_order\Entity\OrderInterface $order
    *   The order entity, or null.
    * @param \Bitpay\InvoiceInterface $invoice
    *   Remote BTCPay invoice.
    *
-   * @return \Drupal\commerce_payment\Entity\PaymentInterface|NULL
+   * @return \Drupal\commerce_payment\Entity\PaymentInterface|null
+   *   The Payment if exists or null otherwise.
    */
   public function loadExistingPayment(OrderInterface $order, InvoiceInterface $invoice);
 
@@ -107,8 +109,6 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
    *   The order entity, or null.
    * @param bool $nonInteractive
    *   Workaround param to handle redirects onNotify() which is non user facing.
-   *
-   *  @return void
    */
   public function redirectOnPaymentError(OrderInterface $order, $nonInteractive = FALSE);
 
@@ -120,20 +120,20 @@ interface BtcPayInterface extends SupportsNotificationsInterface {
    * @param \Bitpay\InvoiceInterface $invoice
    *   Remote BTCPay invoice.
    *
-   * @return \Drupal\commerce_payment\Entity\PaymentInterface|NULL
+   * @return \Drupal\commerce_payment\Entity\PaymentInterface|null
+   *   The Payment entity if successful or null otherwise.
    */
-  public function processPayment($invoice);
+  public function processPayment(InvoiceInterface $invoice);
 
   /**
    * Map the remote payment state to some available Commerce payment state.
    *
-   * TODO: think about how to handle overpayments (duplicate/multiple) payments? Create new payment or update old?
+   * TODO: think about how to handle overpayments (duplicate/multiple)
+   * payments? Create new payment or update old?
    * TODO: maybe add remote payment states to Commerce payment states?
    *
    * @param string $remoteState
    *   Remote BTCPay invoice state.
-   *
-   * @return void
    */
   public function mapRemotePaymentState($remoteState);
 
